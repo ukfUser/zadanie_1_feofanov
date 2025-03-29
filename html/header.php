a960adf3536a8efb4282ecf0f8e6a2577d0b9be4
ukfUser <sniper12@i.ua>
2025-03-29 20:17:46 +0100

    1. Created header.php and footer.php. 2. included those two files in every page i found.

---
+++ html/header.php
@@ -0,0 +1,20 @@
+<header class="site-header d-flex flex-column justify-content-center align-items-center">
+    <div class="container">
+        <div class="row align-items-center">
+
+            <div class="col-lg-5 col-12">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Homepage</a></li>
+
+                        <li class="breadcrumb-item active" aria-current="page">Contact Form</li>
+                    </ol>
+                </nav>
+
+                <h2 class="text-white">Contact Form</h2>
+            </div>
+
+        </div>
+    </div>
+</header>
+
